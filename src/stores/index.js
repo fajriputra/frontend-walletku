40aaecd503e50fca89086572ee69963c3773b7d1
unknown <fajriwn27@gmail.com>
2021-12-07 22:27:53 +0700

    remove redux-logger configuration

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import logger from "redux-logger";
 import promiseMiddleware from "redux-promise-middleware";
 
 import authReducers from "stores/auth/reducers";
@@ -24,10 +23,7 @@ const rootReducers = combineReducers({
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducers);
-const store = createStore(
-  persistedReducer,
-  applyMiddleware(promiseMiddleware, logger)
-);
+const store = createStore(persistedReducer, applyMiddleware(promiseMiddleware));
 
 const persistor = persistStore(store);
 export { store, persistor };
